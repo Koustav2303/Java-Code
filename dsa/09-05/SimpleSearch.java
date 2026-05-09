8cb9680d994b9f7727ca51da588537d02fbb1392
CODEBOT <pankoustav@gmail.com>
2026-05-09 22:50:40 +0530

    feat: add basic linear search implementation

---
+++ dsa/09-05/SimpleSearch.java
@@ -0,0 +1,9 @@
+public class SimpleSearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int target = 30;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == target) System.out.println("Found at: " + i);
+        }
+    }
+}
