d99911ace7e8b71b04c9e26a26aef4b8c00fd4cd
CODEBOT <pankoustav@gmail.com>
2026-05-09 23:10:52 +0530

    feat: add Pascal's Triangle row generator using binomial coefficients

---
+++ dsa/09-05/PowerTwo.java
@@ -0,0 +1,7 @@
+public class PowerTwo {
+    public static void main(String[] args) {
+        int n = 16;
+        boolean isPower = (n > 0) && ((n & (n - 1)) == 0);
+        System.out.println(n + " is power of 2: " + isPower);
+    }
+}
