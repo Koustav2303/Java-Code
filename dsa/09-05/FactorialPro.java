725e6dccbc66e8242868c81f5a01c255e9ecbf2d
CODEBOT <pankoustav@gmail.com>
2026-05-09 22:41:09 +0530

    FactorialPro code added

---
+++ dsa/09-05/FactorialPro.java
@@ -0,0 +1,10 @@
+public class FactorialPro {
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5: " + fact(5));
+    }
+
+    public static int fact(int n) {
+        if (n <= 1) return 1;
+        return n * fact(n - 1);
+    }
+}
